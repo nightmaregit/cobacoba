b94c4c672b69c9c7a798934c0545756fd4025408
nightmaregit <142392592+nightmaregit@users.noreply.github.com>
2024-09-20 21:09:03 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+
+const functions = require("firebase-functions");
+
+// Endpoint untuk mengirimkan firebaseConfig
+exports.getFirebaseConfig = functions.https.onRequest((req, res) => {
+  const firebaseConfig = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+  };
+  res.status(200).send(firebaseConfig);
+});
